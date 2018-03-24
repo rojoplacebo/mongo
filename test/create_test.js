027a3334bee6a8efc8332ce17e9274cfcab18f6a
Rogelio Granillo <kikorojoplacebo@gmail.com>
2018-03-24 00:31:52 -0600

    it implementation

--- test/create_test.js
+++ test/create_test.js
@@ -1,7 +1,12 @@
 'use strict';
+var assert = require('assert');
+const User = require('../src/user');
 
 describe('crear usuarios', () =>{
-    it('crea usuario',() =>{
-        assert(1+2===3);
+    it('crea usuario',() => {
+        const rogelio = new User({
+            'name': 'rogelio'
+        });
+        rogelio.save();
     });
 });
